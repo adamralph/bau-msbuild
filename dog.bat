c5c434d99f43a053cf04768cd6feec2d48fb3b31
Adam Ralph <adam@adamralph.com>
2014-06-02 01:58:56 +0200

    build: update packages docs: tidy up README.md

--- dog.bat
+++ dog.bat
@@ -1,28 +1,9 @@
 @ECHO OFF
 
 ECHO [Dog] WARNING: Ensure you have built using baufile.csx before building with this!
-ECHO [Dog] WARNING: If anything goes wrong here, you may end up with an altered packages.config. If so, revert the changes before doing anything.
-
-ECHO [Dog] Removing Bau.MSBuild package folder...
-rmdir packages\Bau.MSBuild.0.1.0-adhoc /S /Q
 
 ECHO [Dog] Copying Bau.MSBuild from bin\Release...
-xcopy src\Bau.MSBuild\bin\Release\Bau.MSBuild.dll packages\Bau.MSBuild.0.1.0-adhoc\lib\net45\ /Y /Q
-
-ECHO [Dog] Copying Bau.MSBuild NuGet package from artifacts\output...
-xcopy artifacts\output\Bau.MSBuild.0.1.0-adhoc.nupkg packages\Bau.MSBuild.0.1.0-adhoc /Y /Q
-
-ECHO [Dog] Taking copy of original packages.config...
-copy packages.config packages.orig.config /Y
-
-ECHO [Dog] Overwriting packages.config with dog.config...
-copy dog.config packages.config /Y
+xcopy src\Bau.MSBuild\bin\Release\Bau.MSBuild.dll packages\Bau.MSBuild.0.1.0-beta03\lib\net45\ /Y /Q
 
 ECHO [Dog] Running dog.csx...
 scriptcs dog.csx -- %*
-
-ECHO [Dog] Overwriting modified packages.config with original packages.config...
-copy packages.orig.config packages.config /Y
-
-ECHO [Dog] Deleting copy of original packages.config...
-del packages.orig.config
